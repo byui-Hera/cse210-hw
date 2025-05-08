1d4c748693036e95a521bec8847c3217525baad1
byui-Hera <herahysi@gmail.com>
2025-05-08 17:18:20 +0200

    save the changes

--- week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd0b85cf02d2958c24f49046dd1c3e7c560ca74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10c517a3907af2c106b960a38f751727d20708f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
