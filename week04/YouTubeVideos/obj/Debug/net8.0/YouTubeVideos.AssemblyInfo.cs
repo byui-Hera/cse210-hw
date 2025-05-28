977d3cb5f409464f4f457dc2e9d4ce6626d91542
byui-Hera <herahysi@gmail.com>
2025-05-28 14:03:43 +0200

    save the changes

--- week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
+++ week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a60725c19ef83c9bcb410d9d390930ffa523a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aef21ba56c6542e54b4fd6fb97148fb306dd096")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
